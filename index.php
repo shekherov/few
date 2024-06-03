72e3d926e7052c3c33daeca9c0afa1edf7b43430
shekherov/few <ssheherov@gmail.com>
2024-06-04 01:05:50 +0500

    Update README.md

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require 'dbconnect.php';
-$result = $conn->query("SELECT *  FROM categories") ;
+require "dbconnect.php";
+$result = $conn->query("SELECT *  FROM category") ;
 echo "<h2>Категории задач</h2>";
 while ($row = $result->fetch()) {
     echo $row['name']."<br>";

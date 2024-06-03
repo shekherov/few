b15671c87ced1b58c9fee9600201ab2d48899c51
shekherov/few <ssheherov@gmail.com>
2024-06-04 00:29:40 +0500

    Update README.md

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
-use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
-if (file_exists(__DIR__."/.env"))
-{
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+<?php
+require "dbconnect.php";
+$result = $conn->query("SELECT *  FROM category") ;
+echo "<h2>Категории задач</h2>";
+while ($row = $result->fetch()) {
+    echo $row['name']."<br>";
+
 }
